765211695fd7f96edc3a6f93749bc570c57541d6
Daniel Gonzalez <danigonza86@gmail.com>
2015-09-15 16:27:30 +0200

    The configuration is now pass by parameters and ni from file

--- lib/orion/config.rb
+++ lib/orion/config.rb
@@ -5,21 +5,18 @@ module Orion
     class << self
       attr_reader :orion_url
 
-      def load_config(orion_url = nil, orion_limit = nil)
+      def load_config(orion_url, orion_limit)
         begin
-          filename = "#{Rails.root}/config/initializers/fiware_orion_config.rb"
-          if self.check_config_file(filename)
-            require filename
-            @orion_url = (orion_url.nil?) ? ORION_SERVER_IP : orion_url
-            @orion_limit = (orion_limit.nil?) ? ORION_LIMIT : orion_limit
-          else
-            @orion_url = orion_url
-            @orion_limit = orion_limit
-          end
-          {
-              orion_url: @orion_url,
-              orion_limit: @orion_limit,
-          }
+          # filename = "#{Rails.root}/config/initializers/fiware_orion_config.rb"
+          # if self.check_config_file(filename)
+          #   require filename
+          #   orion_url = (orion_url.nil?) ? ORION_SERVER_IP : orion_url
+          #   orion_limit = (orion_limit.nil?) ? ORION_LIMIT : orion_limit
+          # else
+            orion_url = orion_url
+            orion_limit = orion_limit
+          # end
+          { orion_url: orion_url, orion_limit: orion_limit }
         rescue => e
           puts e => e.message
         end
@@ -28,11 +25,11 @@ module Orion
       def check_config_file(filename)
         result = true
         unless File.exist?(filename)
-          puts "|>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-          puts "|"
+          puts '|>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
+          puts '|'
           puts "Please configure orion_url in #{filename}."
-          puts "|"
-          puts "|>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+          puts '|'
+          puts '|>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
           result = false
         end
         result
